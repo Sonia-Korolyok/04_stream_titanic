d7f3dc3f5ae14f3f95a90ddb013f314abcaedce6
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-11-02 19:15:00 +0200

    Refactor

--- model/titanic.js
+++ model/titanic.js
@@ -8,122 +8,81 @@ export class Titanic {
         this.separator = separator;
     }
 
-    totalFares() {
-        const reader = readline.createInterface({
-            input: fs.createReadStream('./train.csv', 'utf8'),
-            crlfDelay: Infinity
-        })
-        let res = 0;
-        let isFirstLine = true
-        reader.on('line', data => {
-            if (isFirstLine) {
-                isFirstLine = false;
-                return;
-            }
-            const cells = data.split(this.separator)
-            res += cells[9] && +cells[9];
-        })
+    async totalFares() {
 
-        reader.on('close', () => {
-            console.log(`Total fares:`, res.toFixed(2));
-        })
-    }
-
-    avgFaresByClasses() {
-        const reader = readline.createInterface({
-            input: fs.createReadStream('./train.csv', 'utf8'),
-            crlfDelay: Infinity
-        })
-        let res = {};
-        let isFirstLine = true
-        reader.on('line', data => {
-            if (isFirstLine) {
-                isFirstLine = false;
-                return;
+        let res= 0;
+        await this._processLines(cells => {
+            if (cells[9]) {
+                res += +cells[9];
             }
-            const cells = data.split(this.separator)
+        })
+        console.log(`Total fares:`, res.toFixed(2));
+     }
+
+    async avgFaresByClasses() {
+        let res= {};
+        await this._processLines(cells => {
             if (cells[9]) {
                 const info = {pClass: cells[2], fare: +cells[9]}
-                const key = info.pClass;
-                if (!res[key]) {
-                    res[key] = [];
-                }
-                res[key].push(info.fare);
+                        const key = info.pClass;
+                        if (!res[key]) {
+                            res[key] = [];
+                        }
+                        res[key].push(info.fare);
             }
         })
-
-        reader.on('close', () => {
-            for (const key in res) {
-                res[key] = +(res[key].reduce((a, b) => a + b) / res[key].length).toFixed(2);
-            }
-            console.log(`Average fares by classes:`, res);
-        })
+        for (const key in res) {
+            res[key] = +(res[key].reduce((a, b) => a + b)/res[key].length).toFixed(2);
+        }
+        console.log(`Average fares by classes:`, res);
     }
 
-    totalSurvived() {
-        const reader = readline.createInterface({
-            input: fs.createReadStream('./train.csv', 'utf8'),
-            crlfDelay: Infinity
-        })
-        let res = {};
-        let isFirstLine = true
-        reader.on('line', data => {
-            if (isFirstLine) {
-                isFirstLine = false;
-                return;
-            }
-            const cells = data.split(this.separator)
-            const key = +cells[1] ? 'Survived' : 'Non survived';
-            if (!res[key]) {
-                res[key] = 0;
-            }
-            res[key]++;
-        })
+    // async avgFaresByClasses() {
+    //     let res = {};
+    //     await this._processLines(cells => {
+    //         if (cells[9]) {
+    //             const info = {pClass: cells[2], fare: +cells[9]}
+    //             const key = info.pClass;
+    //             if (!res[key]) {
+    //                 res[key] = [];
+    //             }
+    //             res[key].push(info.fare);
+    //         }
+    //         for (const key in res) {
+    //             res[key] = +(res[key].reduce((a, b) => a + b) / res[key].length).toFixed(2);
+    //             console.log(`Average fares by classes:`, res);
+    //         }
+    //     })
+    // }
 
-        reader.on('close', () => {
-            console.log(res);
-        })
-    }
+   async totalSurvived() {
+       let res = {};
+       await this._processLines(cells => {
+           const key = +cells[1] ? 'Survived' : 'Non survived';
+           if (!res[key]) {
+               res[key] = 0;
+           }
+           res[key]++;
+       })
+       console.log(res);
+   }
 
-    totalSurvivedByGender() {
-        const reader = readline.createInterface({
-            input: fs.createReadStream('./train.csv', 'utf8'),
-            crlfDelay: Infinity
-        })
+   async totalSurvivedByGender() {
         let res = {};
-        let isFirstLine = true
-        reader.on('line', data => {
-            if (isFirstLine) {
-                isFirstLine = false;
-                return;
-            }
-            const cells = data.split(this.separator)
+        await this._processLines(cells => {
             const key = this._survivedGender(cells[4], cells[1]);
             if (!res[key]) {
                 res[key] = 0;
             }
             res[key]++;
         })
-
-        reader.on('close', () => {
-            console.log(res);
-        })
+       console.log(res);
     }
 
-    totalSurvivedChildren(){
-        const reader = readline.createInterface({
-            input: fs.createReadStream('./train.csv', 'utf8'),
-            crlfDelay: Infinity
-        })
+   async totalSurvivedChildren() {
         let res = {};
-        let isFirstLine = true
-        reader.on('line', data => {
-            if (isFirstLine) {
-                isFirstLine = false;
-                return;
-            }
-            const cells = data.split(this.separator)
-            if(cells[5] && cells[5] < 18) {
+        await this._processLines(cells => {
+            if (cells[5] && cells[5] < 18) {
                 const key = +cells[1] ? 'Children survived' : 'Children non survived';
                 if (!res[key]) {
                     res[key] = 0;
@@ -131,22 +90,36 @@ export class Titanic {
                 res[key]++;
             }
         })
+       console.log(res);
+   }
 
-        reader.on('close', () => {
-            console.log(res);
-        })
-    }
 
-    showStats(){
-        this.totalFares();
-        this.avgFaresByClasses();
-        this.totalSurvived();
-        this.totalSurvivedByGender();
-        this.totalSurvivedChildren();
+   async showStats() {
+       await this.totalFares();
+        await this.avgFaresByClasses();
+        await this.totalSurvived();
+        await this.totalSurvivedByGender();
+        await this.totalSurvivedChildren();
     }
 
     _survivedGender(gender, survived) {
         survived = +survived ? 'survived' : 'non survived';
         return gender + " " + survived;
+    }
+
+    async _processLines(process) {
+        const reader = readline.createInterface({
+            input: fs.createReadStream(this.src, 'utf8'),
+            crlfDelay: Infinity
+        })
+        let isFirstLine = true;
+        for await (const line of reader){
+            if (isFirstLine) {
+                isFirstLine = false;
+                continue;
+            }
+            const cells = line.split(this.separator)
+            process(cells);
+        }
     }
 }
